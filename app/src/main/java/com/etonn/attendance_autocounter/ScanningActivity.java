1924c6c9438c2f5b78b888b5fbaa7c883076843a
Cheng Zhang <zhangchengx@gmail.com>
2014-11-07 15:40:26 -0500

    add Bluetooth detecting and opening

--- app/src/main/java/com/etonn/attendance_autocounter/ScanningActivity.java
+++ app/src/main/java/com/etonn/attendance_autocounter/ScanningActivity.java
@@ -1,17 +1,77 @@
 package com.etonn.attendance_autocounter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.etonn.attendance_autocounter.bluetooth.BluetoothHelper;
+import com.etonn.attendance_autocounter.db.DBManager;
+
+import java.util.ArrayList;
 
 
 public class ScanningActivity extends ActionBarActivity {
+
+    DBManager db;
+    BluetoothHelper bluetooth = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanning);
+
+        // init database
+        db = new DBManager(ScanningActivity.this);
+
+        // detect Bluetooth
+        bluetooth = new BluetoothHelper();
+        if (!bluetooth.detectBluetooth()) {
+            // if user click button, close current activity
+            new AlertDialog.Builder(this)
+                    .setTitle("Message")
+                    .setMessage("Your device does not support Bluetooth.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            ScanningActivity.this.finish();
+                        }
+                    })
+                    .show();
+        }
+
+        // open Bluetooth
+        if (!bluetooth.isBluetoothOpening()) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Warning!")
+                    .setMessage("Please turn on your Bluetooth.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            // open Bluetooth
+                            bluetooth.openBluetooth();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            // return MainActivity
+                            ScanningActivity.this.finish();
+                        }
+                    })
+                    .show();
+        }
+
+        // scan Bluetooth device
+        Toast.makeText(getApplicationContext(), "Scanning Students...", Toast.LENGTH_SHORT).show();
+        ArrayList btlist = bluetooth.getBluetoothList();
     }
 
 
@@ -36,4 +96,5 @@ public class ScanningActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
